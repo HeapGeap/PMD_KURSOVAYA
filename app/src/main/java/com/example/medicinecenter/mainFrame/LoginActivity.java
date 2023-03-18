a9e1b4f23cc0a4a66144db99bb113abb3f7e7516
Илья <vitilyusha@gmail.com>
2023-03-18 20:49:11 +0300

    First steps

---
+++ app/src/main/java/com/example/medicinecenter/mainFrame/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.medicinecenter.mainFrame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medicinecenter.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
