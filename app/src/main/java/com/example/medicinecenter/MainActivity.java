18fe9dce0707ba1b44747a279d529cb9a78abc48
Илья <vitilyusha@gmail.com>
2023-03-22 00:14:07 +0300

    after auth window UI(Start)

--- app/src/main/java/com/example/medicinecenter/MainActivity.java
+++ app/src/main/java/com/example/medicinecenter/MainActivity.java
@@ -5,65 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity  {
-    int signTries = 0;
+
+
+    private ProgressBar homeBar;
+    private double Progress_Status = 0;
+    private Handler homeHandler = new Handler();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        auth_block();
+        setContentView(R.layout.activity_main);
+        progBarAnimation();
 
 
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+              startActivity(intent);
+              finish();
+          }
+      },5000);
     }
 
-
-    public void auth_block(){
-
-        final Button auth = (Button) findViewById(R.id.auth_button);
-        final Button reg = (Button) findViewById(R.id.reg);
-
-        EditText name = findViewById(R.id.name_field);
-        EditText pass = findViewById(R.id.pass_field);
-
-
-        auth.setOnClickListener(new View.OnClickListener() {
+    public void progBarAnimation(){
+        homeBar = (ProgressBar) findViewById(R.id.progressBar);
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-
-                if(name.getText().toString().equals("qwert") && pass.getText().toString().equals("12345")){
-                    Intent intent = new Intent(MainActivity.this, UserProfile.class);
-                    startActivity(intent);
-                    signTries = 0;
-                }
-
-                if( (name.getText().length()) != 0 && (pass.getText().length()) != 0){
-                    signTries++;
-                    if(signTries == 7){
-                        auth.setEnabled(false);
-                    }
+            public void run() {
+                while(Progress_Status < 100){
+                    Progress_Status+=1;
+                    android.os.SystemClock.sleep(50);
+                    homeHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            homeBar.setProgress((int)Progress_Status);
+                        }
+                    });
                 }
             }
-
-        });
-
-        reg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Registration.class);
-                startActivity(intent);
-            }
-        });
-
+        }).start();
     }
-
 
 
 }
